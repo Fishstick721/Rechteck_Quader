30500db53e62743654b1aa0263b9b11eb2b6debe
tobias-htl-wo <tobias.schober@htl-wolfsberg.at>
2023-06-28 09:15:54 +0200

    Projektdateien hinzufügen.

---
+++ Rechteck_Quader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rechteck_Quader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
